af8274b2c647c079139d84702b48a005d772da61
ymislu <ymislu@163.com>
2025-03-19 16:56:45 +0800

    feat: Initializer of anonymous and workflows.

--- api/initializer/__init__.py
+++ api/initializer/__init__.py
@@ -1,13 +1,9 @@
-from typing import Callable, List, Tuple
 from flask import Flask
-
-_initializers: List[Tuple[Callable, int]] = []
-
-def initializer(priority: int = 10) -> Callable:
-    def decorator(func: Callable) -> Callable:
-        _initializers.append((func, priority))
-        return func
-    return decorator
+from . import (
+    account,
+    workflow
+)
+from .decorator import _initializers
 
 def run_initializers(app: Flask):
     with app.app_context():
