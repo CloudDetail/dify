a8cbd0d29498223a84e077615d1bc3767e0a8097
misluyu <ymislu@163.com>
2025-10-30 19:24:18 +0800

    fix: Use backend to query metrics instead of vm. Add data evidence.

--- api/core/tools/builtin_tool/providers/apo_select/tools/polaris_time_by_pid.py
+++ api/core/tools/builtin_tool/providers/apo_select/tools/polaris_time_by_pid.py
@@ -19,66 +19,51 @@ class PolarisTimeByPidTool(BuiltinTool):
         app_id: Optional[str] = None,
         message_id: Optional[str] = None,
     ) -> Generator[ToolInvokeMessage, None, None]:
-        pid = tool_parameters.get('pid', '.*')
         start_time = tool_parameters.get("startTime")
         end_time = tool_parameters.get("endTime")
-        resource_type = tool_parameters.get('type', '.*')
-        step = APOUtils.get_step(start_time=start_time, end_time=end_time)
-        interval = APOUtils.vec_from_duration(step * 1000)
-        if len(pid) == 0:
-            pid = '.*'
-        if len(resource_type) == 0:
-            resource_type = '.*'
+        resource_type = tool_parameters.get('type', '')
 
-        query = f"""
-        increase(
-          sum without (tid_key) (
-            originx_thread_polaris_nanoseconds_sum{{
-              container_id!="",
-              type=~"{resource_type}",
-              pid=~"{pid}"
-            }}[{interval}]
-          )
-        )
-        """
-        resp = requests.get(
-            dify_config.APO_VM_URL + "/prometheus/api/v1/query_range",
-            params={
-                'query': query,
-                'start': start_time / 1000,
-                'end': end_time / 1000,
-                'step': APOUtils.get_step_with_unit(start_time, end_time)
-            }
-        ).json()
+        pid = tool_parameters.get('pid', '')
+        node = tool_parameters.get('node', '')
+        pids = pid.split('|')
+        nodes = node.split('|')
 
-        timeseries = []
-        for res in resp.get('data', {}).get('result', []):
-            labels = res.get('metric')
-            chart_data = {}
-            for pair in res.get('values', []):
-                if len(pair) < 2:
-                    continue
-                try:
-                    value = float(pair[1])
-                    chart_data[pair[0] * 1_000_000] = value
-                except (ValueError, TypeError) as e:
-                    continue
-            timeseries.append(
-                {
-                    "labels": labels,
-                    "legend": f"{labels.get('pid')}",
-                    "chart": {
-                        "chartData": chart_data,
-                    }
-                }
-            )      
-        # timeseries数组 [{"labels": {}, "chart:{"chartData": {"time": value}"}}]
+        if resource_type == 'cpu':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - OnCPU'
+        elif resource_type == 'net':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - Net'
+        elif resource_type == 'runq':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - Runqueue'
+        elif resource_type == 'idle':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - Idle'
+        elif resource_type == 'other':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - Other'
+        elif resource_type == 'epoll':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - Epoll'
+        elif resource_type == 'file':
+            metric_name = 'Thread Polaris Metrics - 北极星指标（进程） - 各类型耗时折线图 - File'
+        params = {
+            'metricName': metric_name,
+            'params': {},
+            'startTime': start_time,
+            'endTime': end_time,
+            'step': APOUtils.get_step(start_time, end_time),
+        }
+        resp = requests.post(dify_config.APO_BACKEND_URL +
+                             '/api/metric/query', json=params)
+        timeseries = resp.json()['result']['timeseries']
+        result = []
+        for ts in timeseries:
+            labels = ts['labels']
+            if labels['pid'] in pids and (not nodes or labels['node'] in nodes):
+                result.append(ts)
+
         resp_json = json.dumps({
             'type': 'metric',
             'display': True,
             'unit': 'us',
             'data': {
-                'timeseries': timeseries
+                'timeseries': result
             }
         })
         yield self.create_text_message(resp_json)
